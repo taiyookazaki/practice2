8662fcac6c8509f254978f491fc378d95625907a
taiyookazaki <0cdi2222@mail.u-tokai.ac.jp>
2022-12-09 12:56:44 +0900

    9-1

--- database/migrations/2022_12_09_125236_add_image_column_to_posts_table.php
+++ database/migrations/2022_12_09_125236_add_image_column_to_posts_table.php
@@ -6,23 +6,12 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
             //
         });
     }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::table('posts', function (Blueprint $table) {
